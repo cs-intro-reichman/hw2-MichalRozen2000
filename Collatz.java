8c084829d87b317f38282feb2cf76b44e8ac0299
MichalRozen2000 <michal.rozen@post.runi.ac.il>
2024-11-21 19:34:22 +0200

    Add files via upload

--- Collatz.java
+++ Collatz.java
@@ -6,26 +6,28 @@ public class Collatz {
 		boolean verbose = mode.equals("v");
 		boolean concise = mode.equals("c");
 		int number = 0;
+		int count = 0;
 
 		if (verbose) {
 			for (int i=1; i<=N; i++){
 				number = i;
-				System.out.println(number+" ");
-				for (int j=1; i>=1; j++){
-					if (number%2 ==0){
-						number /=2;
-					} else {
-						number = (number*3)+1; 
-					} if (number != 1){
-						System.out.println(number + " ");
-					} else {
-						System.out.println (number + "(" + (j+1) + ")");
-						break;
+				System.out.print(number+" ");
+				while (number != 1) {  
+                    if (number % 2 == 0) {
+                        number /= 2; 
+                    } else {
+                        number = (number * 3) + 1; 
+                    }
+                    System.out.print(number + " "); 
+                    count++;
+				
 					}
+					System.out.println("(" + (count+1) + ")");
+
 				}
 			}
 		}
 
 	}
-}
+
 	
