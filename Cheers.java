40641f90bb86c01fb66e3fc3bd639cd9f4bfdfe9
MichalRozen2000 <michal.rozen@post.runi.ac.il>
2024-11-21 10:27:36 +0200

    Add files via upload

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,36 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
-}
+                String word = (args[0]).toUpperCase();
+                int times = Integer.parseInt(args[1]);
+                int spelling = 0;
+                int count = 0;
+                String tone = " ";
+                String specialLetters = "AEFHILMNORSXaefhilmnorsx";       
+                
+    
+                while (spelling < word.length()) {
+                    char Letter = word.charAt(spelling);
+    
+                    tone = "a";  
+                    if (specialLetters.indexOf(Letter) != -1) {
+                        tone = "an";  
+                    }
+    
+                    
+                    System.out.println("Give me " +tone + " " + word.charAt(spelling) + ": " + word.charAt(spelling) + "!");
+                    spelling ++;
+                    }
+                
+                System.out.println("What does that spell?");
+    
+                
+                while (count < times){
+                    System.out.println(word + "!!!");
+                    count++;
+                }
+              }
+            }
+    
+        
+
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,31 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt (args[0]);
+		String mode = args[1];
+		boolean verbose = mode.equals("v");
+		boolean concise = mode.equals("c");
+		int number = 0;
+
+		if (verbose) {
+			for (int i=1; i<=N; i++){
+				number = i;
+				System.out.println(number+" ");
+				for (int j=1; i>=1; j++){
+					if (number%2 ==0){
+						number /=2;
+					} else {
+						number = (number*3)+1; 
+					} if (number != 1){
+						System.out.println(number + " ");
+					} else {
+						System.out.println (number + "(" + (j+1) + ")");
+						break;
+					}
+				}
+			}
+		}
+
 	}
 }
+	
